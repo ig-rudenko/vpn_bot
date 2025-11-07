f031240ce4fdcbb6a68ede26d3a1657da23fd046
Игорь Руденко <ig.rudenko1@yandex.ru>
2025-11-07 16:04:34 +0300

    Add notification webhook

--- notification_webhook.py
+++ notification_webhook.py
@@ -9,16 +9,25 @@ from database.connection import db
 app = web.Application()
 
 BOT_TOKEN = os.getenv("TG_TOKEN")
+ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",")]
+
 db.init("sqlite+aiosqlite:///db.sqlite3")
+
+
+async def send_tg_message(session: aiohttp.ClientSession, tg_id: int, message: str):
+    async with session.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        json={"chat_id": tg_id, "text": message, "parse_mode": "HTML"},
+    ) as response:
+        if response.status != 200:
+            print("tg_id", tg_id, "message", message, await response.text())
 
 
 async def send_notification(tg_id: int, message: str):
     async with aiohttp.ClientSession() as session:
-        async with session.post(
-                f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-                json={"chat_id": tg_id, "text": message, "parse_mode": "HTML"},
-        ) as response:
-            print(response.status)
+        await send_tg_message(session, tg_id, message)
+        for admin_id in ADMIN_IDS:
+            await send_tg_message(session, admin_id, message)
 
 
 class WebhookView(web.View):
@@ -34,7 +43,6 @@ class WebhookView(web.View):
         message = f"""
 ❌ Вы были заблокированы за использование Торрента
 🕗 Время блокировки: {data.get('duration', '-')} мин.
-🤔 {data.get('action', '-')}
 🌐 IP: {data.get('ip', '-')}""".strip()
 
         await send_notification(tg_id=vpn_connection.tg_id, message=message)

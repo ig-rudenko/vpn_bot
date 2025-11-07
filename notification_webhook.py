466edae9927d22ba88e9f0ec55a1f18a078b06d8
Игорь Руденко <ig.rudenko1@yandex.ru>
2025-11-07 15:55:33 +0300

    Add notification webhook

---
+++ notification_webhook.py
@@ -0,0 +1,47 @@
+import os
+
+import aiohttp
+from aiohttp import web
+
+from app.models import VPNConnection
+from database.connection import db
+
+app = web.Application()
+
+BOT_TOKEN = os.getenv("TG_TOKEN")
+db.init("sqlite+aiosqlite:///db.sqlite3")
+
+
+async def send_notification(tg_id: int, message: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+                f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+                json={"chat_id": tg_id, "text": message, "parse_mode": "HTML"},
+        ) as response:
+            print(response.status)
+
+
+class WebhookView(web.View):
+    async def post(self):
+        data: dict = await self.request.json()
+        # {"username":"%s","ip":"%s","server":"%s","action":"%s","duration":%d,"timestamp":"%s"}
+
+        try:
+            vpn_connection = await VPNConnection.get(username=data["username"])
+        except VPNConnection.DoesNotExists:
+            return web.Response(text="OK")
+
+        message = f"""
+❌ Вы были заблокированы за использование Торрента
+🕗 Время блокировки: {data.get('duration', '-')} мин.
+🤔 {data.get('action', '-')}
+🌐 IP: {data.get('ip', '-')}""".strip()
+
+        await send_notification(tg_id=vpn_connection.tg_id, message=message)
+        return web.Response(text="OK")
+
+
+app.add_routes([web.post("/webhook", WebhookView)])
+
+if __name__ == "__main__":
+    web.run_app(app, host="127.0.0.1", port=8080)
